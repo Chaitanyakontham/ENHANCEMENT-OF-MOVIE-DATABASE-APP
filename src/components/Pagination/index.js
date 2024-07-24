5f9e5df085a14a70c337f650b43171d6ed59c70d
Chaitanyakontham <konthamchaitanya234@gmail.com>
2024-07-24 09:06:03 +0000

    solution

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -44,15 +44,15 @@ class Pagination extends React.Component {
 
   render() {
     const {pageNo} = this.state
-    const {totalPages} = this.props
+    // const {totalPages} = this.props
 
     return (
-      <div className='mb-3 d-flex justify-content-center align-items-center'>
-        <button type='button' className='control-btn' onClick={this.onPrevPage}>
+      <div className="mb-3 d-flex justify-content-center align-items-center">
+        <button type="button" className="control-btn" onClick={this.onPrevPage}>
           Prev
         </button>
-        <p className='page-no'>{pageNo}</p>
-        <button type='button' className='control-btn' onClick={this.onNextPage}>
+        <p className="page-no">{pageNo}</p>
+        <button type="button" className="control-btn" onClick={this.onNextPage}>
           Next
         </button>
       </div>
